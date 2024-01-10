7c926e4437395fa297cc5b2060dcbde0dab8ec0c
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-09 20:38:37 -0500

    nt1 se implementa algoritmo de datos primitvos y suma de consola

---
+++ main.py
@@ -0,0 +1,16 @@
+#VARIABLES PRIMITIVAS CON PYTHON 
+#QUEMAS LAS VARIABLES 
+
+nombreEstudiante = 'Jorge Esteban Cardona Lopez'
+edadEstudiante = 21
+estaturaEstudiante = 1.84
+hinchaDelMejorEquipo = True
+
+#PEDIR DATOS POR TECLADO
+ 
+comidaFavorita = input('Cual es tu comida favorita?')
+
+#MOSTRAR DATOS EN PANTALLA (SALIDA)
+
+
+print(F"Hola {nombreEstudiante} tu comida favorita es {comidaFavorita}")
